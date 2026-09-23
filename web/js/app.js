a76047d5ca7c0ce0c72cdc68c2577e2e9ab8d238
Vure-sh <alimohammed42829@gmail.com>
2026-09-23 16:51:17 +0300

    fix(ui): simplify English subtitle dropdown option label

--- web/js/app.js
+++ web/js/app.js
@@ -46,7 +46,7 @@ const AUDIO_OPTIONS = [
 
 const SUBS_OPTIONS = [
   { val: 'all', label: 'All available' },
-  { val: 'en-US', label: 'English (+ CC)' },
+  { val: 'en-US', label: 'English' },
   { val: 'en-US-cc', label: 'English (CC)' },
   { val: 'ja-JP', label: 'Japanese' },
   { val: 'es-419', label: 'Spanish (Latin America)' },
